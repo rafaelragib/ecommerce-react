351e58091bf864e1765fcb91831262fb1c92c580
Ragib <ragib08@gmail.com>
2021-03-12 01:12:59 +0600

    API added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import React,{useState,useEffect} from 'react'
 import {Products,Navbar} from "./components/";
+import {commerce} from './lib/commerce'
+const App= ()=> 
+{
+  const [products,setProducts]= useState([]);
 
-function App() {
+  const fetchProduct = async () =>
+  {
+    const {data} =await commerce.products.list();
+    setProducts(data);
+  }
+
+  useEffect(() => {
+    fetchProduct();
+  }, [])
+  console.log(products);
   return (
     <div>
     <Navbar/>
